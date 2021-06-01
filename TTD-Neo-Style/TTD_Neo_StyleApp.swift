36d6bbc3e2f3794957d80e801ff725cc5f496670
Daniel Enning <enningm28@gmail.com>
2021-06-01 16:51:16 +0200

    Initial Commit

---
+++ TTD-Neo-Style/TTD_Neo_StyleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TTD_Neo_StyleApp.swift
+//  TTD-Neo-Style
+//
+//  Created by Daniel Enning on 01.06.21.
+//
+
+import SwiftUI
+
+@main
+struct TTD_Neo_StyleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
